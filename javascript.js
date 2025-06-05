e9c9830f9e29ccccf72c350d79b67c9b3e3e963d
ilyass <ilyassgoumi11@gmail.com>
2025-06-05 12:04:13 +0200

    deploy

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+  const icon = document.querySelector(".menu-bar i");
+   const list = document.querySelector(".nav");
+   const close = document.querySelector(".nav li a");
+
+   icon.onclick = () => {
+        icon.classList.toggle('fa-xmark');
+        list.classList.toggle('active');
+       
+    }
+    
+ 
+  
+  
+  
+  
+  
+  
+  
+   
